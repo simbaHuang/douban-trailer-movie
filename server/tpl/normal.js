da2cdac717df10d7b13a880bb494bb3f811766e7
simbaHuang <hx19920411@hotmail.com>
2019-06-28 10:33:58 +0800

    feat：新增html静态文件

---
+++ server/tpl/normal.js
@@ -0,0 +1,26 @@
+module.exports = `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial=scale=1"/>
+			<title>Koa Server HTML</title>
+
+			<link rel="stylesheet" href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" />
+			<script src="https://cdn.bootcss.com/jquery/3.4.0/jquery.min.js"></script>
+			<script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+		</head>
+		<body>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>HI SIMBA</h1>
+					</div>
+					<div class="col-md-4">
+						啦啦啦
+					</div>
+				</div>
+			</div>
+		</body>
+	</html>
+`
